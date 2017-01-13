e3f548d5a0691d734132dd330f05a13945f1b137
jota_sk <jota_sk@hotmail.com>
2017-01-13 23:28:51 +0000

    Initial commit

---
+++ android/src/com/github/jotask/rosjam/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.jotask.rosjam;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.jotask.rosjam.Rosjam;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Rosjam(), config);
+	}
+}
---
+++ desktop/src/com/github/jotask/rosjam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.jotask.rosjam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.jotask.rosjam.Rosjam;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Rosjam(), config);
+	}
+}
