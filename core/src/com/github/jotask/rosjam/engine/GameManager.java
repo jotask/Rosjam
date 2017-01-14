9f398730bc26677083b1c3ae21e24b9397ab5f9e
jota_sk <jota_sk@hotmail.com>
2017-01-14 00:07:28 +0000

    basic game manager

---
+++ core/src/com/github/jotask/rosjam/engine/GameManager.java
@@ -0,0 +1,80 @@
+package com.github.jotask.rosjam.engine;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.github.jotask.rosjam.engine.states.State;
+import com.github.jotask.rosjam.game.Game;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * GameManager
+ *
+ * @author Jose Vives Iznardo
+ * @since 13/01/2017
+ */
+public class GameManager extends State{
+
+    public enum STATE { DUNGEON }
+
+    private State currentState;
+
+    public GameManager() {
+        this.changeState(STATE.DUNGEON);
+    }
+
+    @Override
+    public void init() { }
+
+    @Override
+    public void preUpdate() { currentState.preUpdate(); }
+
+    @Override
+    public void postUpdate() { currentState.postUpdate(); }
+
+    @Override
+    public void preRender(SpriteBatch sb) { currentState.preRender(sb); }
+
+    @Override
+    public void postRender(SpriteBatch sb) { currentState.postRender(sb); }
+
+    @Override
+    public void preDebug(ShapeRenderer sr) { currentState.preDebug(sr); }
+
+    @Override
+    public void postDebug(ShapeRenderer sr) { currentState.postDebug(sr); }
+
+    @Override
+    public void update() { currentState.update(); }
+
+    @Override
+    public void render(SpriteBatch sb){ currentState.render(sb); }
+
+    @Override
+    public void debug(ShapeRenderer sr){ currentState.debug(sr); }
+
+    @Override
+    public void dispose(){
+        currentState.dispose();
+    }
+
+    public void changeState(STATE state){
+        if(currentState != null)
+            currentState.dispose();
+
+        // TODO implement loading screen
+
+        State s = null;
+        switch (state){
+            case DUNGEON:
+                s = new Game();
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+
+        currentState = s;
+        currentState.init();
+
+    }
+
+}
--- core/src/com/github/jotask/rosjam/game/Game.java
+++ core/src/com/github/jotask/rosjam/game/Game.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.github.jotask.rosjam.engine.GameManager;
 import com.github.jotask.rosjam.engine.states.State;
 
 /**
@@ -14,29 +15,50 @@ import com.github.jotask.rosjam.engine.states.State;
  */
 public class Game extends State {
 
+    private GameManager gameManager;
+
     @Override
     public void init() {
         this.camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        this.gameManager = new GameManager();
     }
 
     @Override
-    public void update() {
+    public void preUpdate() { this.gameManager.preUpdate(); }
 
+    @Override
+    public void postUpdate() { this.gameManager.postUpdate(); }
+
+    @Override
+    public void preRender(SpriteBatch sb) { this.gameManager.preRender(sb); }
+
+    @Override
+    public void postRender(SpriteBatch sb) { this.gameManager.postRender(sb); }
+
+    @Override
+    public void preDebug(ShapeRenderer sr) { this.gameManager.preDebug(sr); }
+
+    @Override
+    public void postDebug(ShapeRenderer sr) { this.gameManager.postDebug(sr); }
+
+    @Override
+    public void update() {
+        this.gameManager.update();
     }
 
     @Override
     public void render(SpriteBatch sb) {
-
+        this.gameManager.render(sb);
     }
 
     @Override
     public void debug(ShapeRenderer sr) {
-
+        this.gameManager.debug(sr);
     }
 
     @Override
     public void dispose() {
-
+        this.gameManager.dispose();
     }
 
 }
