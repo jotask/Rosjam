931e4f6d10a50e7c9ccf7b6ed7d5d5e01999a3b2
jota_sk <jota_sk@hotmail.com>
2017-01-22 01:39:36 +0000

    class for a timer

---
+++ core/src/com/github/jotask/rosjam/util/Timer.java
@@ -0,0 +1,48 @@
+package com.github.jotask.rosjam.util;
+
+/**
+ * Timer
+ *
+ * @author Jose Vives Iznardo
+ * @since 21/01/2017
+ */
+public class Timer {
+
+    private long time;
+    private long next;
+
+    public Timer(float seg){
+
+        time = this.toSec(seg);
+
+        reset();
+    }
+
+    private long toSec(float seg){
+        Float f = new Float(seg);
+        long s = f.longValue();
+
+        s *= 1000;
+        s *= 1000;
+        s *= 1000;
+
+        return s;
+
+    }
+
+    public void reset(){
+        this.next = System.nanoTime() + time;
+    }
+
+    public boolean isPassed(){ return this.isPassed(false);}
+    public boolean isPassed(boolean reset){
+
+        boolean b = (System.nanoTime() > this.next);
+
+        if(reset && b)
+            reset();
+
+        return b;
+    }
+
+}
