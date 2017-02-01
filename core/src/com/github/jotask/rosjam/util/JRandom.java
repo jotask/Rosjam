630fdbeb5975ede208a3c524a554f637059f2c41
jota_sk <jota_sk@hotmail.com>
2017-02-01 13:35:25 +0000

    create my own random class

---
+++ core/src/com/github/jotask/rosjam/util/JRandom.java
@@ -0,0 +1,30 @@
+package com.github.jotask.rosjam.util;
+
+import com.badlogic.gdx.math.MathUtils;
+
+import java.util.Random;
+
+/**
+ * Generator
+ *
+ * @author Jose Vives Iznardo
+ * @since 31/01/2017
+ */
+public final class JRandom {
+
+    private final Random random;
+    private final long seed;
+
+    public JRandom(){ this(MathUtils.random(Long.MAX_VALUE)); }
+
+    public JRandom(long seed) {
+        this.seed = seed;
+        this.random = new Random();
+        this.random.setSeed(seed);
+    }
+
+    public int random(int range){
+        return random.nextInt(range);
+    }
+
+}
