4b8fc6bf9388561361bb6de14c22a4af7604889a
jota_sk <jota_sk@hotmail.com>
2017-02-08 18:53:17 +0000

    added assets for the enemies

---
+++ core/src/com/github/jotask/rosjam/engine/assets/EnemyAssets.java
@@ -0,0 +1,53 @@
+package com.github.jotask.rosjam.engine.assets;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.github.jotask.rosjam.game.entity.enemy.Enemies;
+
+import java.util.HashMap;
+
+/**
+ * EnemyAssets
+ *
+ * @author Jose Vives Iznardo
+ * @since 08/02/2017
+ */
+public class EnemyAssets {
+
+    private final String filename = "enemies.png";
+    private final Class<?> type = Texture.class;
+
+    private Texture texture;
+    private final Assets assets;
+
+    // TODO implement m owm map to choose random regions with the same key
+    private HashMap<Enemies, TextureRegion> map;
+
+    public EnemyAssets(final Assets assets) {
+        this.assets = assets;
+        this.assets.getAssetManager().load(filename, type);
+        this.map = new HashMap<Enemies, TextureRegion>();
+    }
+
+    public void prepare(){
+        texture = (Texture) this.assets.getAssetManager().get(this.filename, this.type);
+        for(Enemies t: Enemies.values())
+            addTile(t);
+    }
+
+    private void addTile(final Enemies tile){
+        this.map.put(tile, generateRegion(tile));
+    }
+
+    private TextureRegion generateRegion(final Enemies tile){
+        TextureRegion region = new TextureRegion();
+        region.setTexture(this.texture);
+        region.setRegion(tile.x, tile.y, tile.width, tile.height);
+        return region;
+    }
+
+    public TextureRegion getRegion(final Enemies key){
+        return this.map.get(key);
+    }
+
+}
