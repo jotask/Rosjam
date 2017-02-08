a9b75a3e099779e01a6e4f7725fdf89eb0ab315f
jota_sk <jota_sk@hotmail.com>
2017-02-08 18:54:05 +0000

    preparing for the new system for the enemies

--- core/src/com/github/jotask/rosjam/game/entity/obstacle/Rock.java
+++ core/src/com/github/jotask/rosjam/game/entity/obstacle/Rock.java
@@ -1,9 +1,10 @@
-package com.github.jotask.rosjam.game.entity;
+package com.github.jotask.rosjam.game.entity.obstacle;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.github.jotask.rosjam.game.entity.BodyEntity;
 
 /**
  * Rock
